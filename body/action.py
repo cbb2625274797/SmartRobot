240b7f2ae253237589d4ee59f3fdafd708f3b412
cbb <2625274797@qq.com>
2025-01-01 20:38:29 +0800

    优化了动作的速度

--- body/action.py
+++ body/action.py
@@ -11,12 +11,12 @@ import threading
 #     father_robot.set_larm_rotation(90)
 #     father_robot.set_rarm_rotation(90)
 #
-#     father_robot.set_foot_rotation(70, 2)
-#     father_robot.set_larm_rotation(50, 3)
-#     father_robot.set_rarm_rotation(50, 3)
-#     father_robot.set_foot_rotation(110, 2)
-#     father_robot.set_larm_rotation(130, 3)
-#     father_robot.set_rarm_rotation(130, 3)
+#     father_robot.set_foot_rotation(70, 4)
+#     father_robot.set_larm_rotation(50, 6)
+#     father_robot.set_rarm_rotation(50, 6)
+#     father_robot.set_foot_rotation(110, 4)
+#     father_robot.set_larm_rotation(130, 6)
+#     father_robot.set_rarm_rotation(130, 6)
 #
 #     father_robot.set_body_rotation(90)
 #     father_robot.set_larm_rotation(90)
@@ -32,22 +32,22 @@ def happy_action(father_robot: robot.ROBOT):
     def thread_function_2():
         father_robot.set_foot_rotation(90)
         # father_robot.set_foot_rotation(70, 1.5)
-        # time.sleep(1)
+        # time.sleep(2)
         # father_robot.set_foot_rotation(110, 1.5)
         father_robot.set_foot_rotation(90)
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(50, 2)
-        time.sleep(1)
-        father_robot.set_larm_rotation(130, 2)
+        father_robot.set_larm_rotation(50, 4)
+        time.sleep(2)
+        father_robot.set_larm_rotation(130, 4)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(130, 2)
-        time.sleep(1)
-        father_robot.set_rarm_rotation(50, 2)
+        father_robot.set_rarm_rotation(130, 4)
+        time.sleep(2)
+        father_robot.set_rarm_rotation(50, 4)
         father_robot.set_rarm_rotation(90)
 
     thread1 = threading.Thread(target=thread_function_1, args=())
@@ -56,11 +56,11 @@ def happy_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
     thread1.join()
@@ -76,26 +76,26 @@ def scare_action(father_robot: robot.ROBOT):
 
     def thread_function_2():
         father_robot.set_foot_rotation(90)
-        father_robot.set_foot_rotation(85, 2)
-        father_robot.set_foot_rotation(95, 2)
-        father_robot.set_foot_rotation(85, 2)
-        father_robot.set_foot_rotation(95, 2)
+        father_robot.set_foot_rotation(85, 4)
+        father_robot.set_foot_rotation(95, 4)
+        father_robot.set_foot_rotation(85, 4)
+        father_robot.set_foot_rotation(95, 4)
         father_robot.set_foot_rotation(90)
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(85, 2)
-        father_robot.set_larm_rotation(95, 2)
-        father_robot.set_larm_rotation(85, 2)
-        father_robot.set_larm_rotation(95, 2)
+        father_robot.set_larm_rotation(85, 4)
+        father_robot.set_larm_rotation(95, 4)
+        father_robot.set_larm_rotation(85, 4)
+        father_robot.set_larm_rotation(95, 4)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(85, 2)
-        father_robot.set_rarm_rotation(95, 2)
-        father_robot.set_rarm_rotation(85, 2)
-        father_robot.set_rarm_rotation(95, 2)
+        father_robot.set_rarm_rotation(85, 4)
+        father_robot.set_rarm_rotation(95, 4)
+        father_robot.set_rarm_rotation(85, 4)
+        father_robot.set_rarm_rotation(95, 4)
         father_robot.set_rarm_rotation(90)
 
     thread1 = threading.Thread(target=thread_function_1, args=())
@@ -104,11 +104,11 @@ def scare_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
 
@@ -129,16 +129,16 @@ def angry_action(father_robot: robot.ROBOT):
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(135, 3)
-        time.sleep(1)
-        father_robot.set_larm_rotation(60, 3)
+        father_robot.set_larm_rotation(135, 6)
+        time.sleep(2)
+        father_robot.set_larm_rotation(60, 6)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(135, 3)
-        time.sleep(1)
-        father_robot.set_rarm_rotation(60, 3)
+        father_robot.set_rarm_rotation(135, 6)
+        time.sleep(2)
+        father_robot.set_rarm_rotation(60, 6)
         father_robot.set_rarm_rotation(90)
 
     thread1 = threading.Thread(target=thread_function_1, args=())
@@ -147,11 +147,11 @@ def angry_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
 
@@ -170,22 +170,22 @@ def upset_action(father_robot: robot.ROBOT):
 
     def thread_function_2():
         father_robot.set_foot_rotation(90)
-        # father_robot.set_foot_rotation(60, 0.75)
-        # father_robot.set_foot_rotation(120, 0.75)
+        # father_robot.set_foot_rotation(60, 1.5)
+        # father_robot.set_foot_rotation(120, 1.5)
         # time.sleep(5)
         father_robot.set_foot_rotation(90)
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(75, 0.75)
-        father_robot.set_larm_rotation(105, 0.75)
+        father_robot.set_larm_rotation(75, 1.5)
+        father_robot.set_larm_rotation(105, 1.5)
         time.sleep(5)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(75, 0.75)
-        father_robot.set_rarm_rotation(105, 0.75)
+        father_robot.set_rarm_rotation(75, 1.5)
+        father_robot.set_rarm_rotation(105, 1.5)
         time.sleep(5)
         father_robot.set_rarm_rotation(90)
 
@@ -195,11 +195,11 @@ def upset_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
 
@@ -212,27 +212,27 @@ def upset_action(father_robot: robot.ROBOT):
 def laugh_action(father_robot: robot.ROBOT):
     def thread_function_1():
         father_robot.set_body_rotation(90)
-        father_robot.set_body_rotation(85, 1)
-        father_robot.set_body_rotation(95, 1)
+        father_robot.set_body_rotation(85, 2)
+        father_robot.set_body_rotation(95, 2)
         father_robot.set_body_rotation(90)
 
     def thread_function_2():
         father_robot.set_foot_rotation(90)
-        father_robot.set_foot_rotation(95, 0.5)
+        father_robot.set_foot_rotation(95, 1)
         father_robot.set_foot_rotation(90)
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(130, 3)
-        time.sleep(1)
-        father_robot.set_larm_rotation(60, 1)
+        father_robot.set_larm_rotation(130, 6)
+        time.sleep(2)
+        father_robot.set_larm_rotation(60, 2)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(130, 1)
-        time.sleep(1)
-        father_robot.set_rarm_rotation(60, 3)
+        father_robot.set_rarm_rotation(130, 2)
+        time.sleep(2)
+        father_robot.set_rarm_rotation(60, 6)
         father_robot.set_rarm_rotation(90)
 
     thread1 = threading.Thread(target=thread_function_1, args=())
@@ -241,11 +241,11 @@ def laugh_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
 
@@ -262,23 +262,23 @@ def curious_action(father_robot: robot.ROBOT):
 
     def thread_function_2():
         father_robot.set_foot_rotation(90)
-        father_robot.set_foot_rotation(85)
-        time.sleep(1)
-        father_robot.set_foot_rotation(95)
+        father_robot.set_foot_rotation(85,3)
+        time.sleep(2)
+        father_robot.set_foot_rotation(95,3)
         father_robot.set_foot_rotation(90)
 
     def thread_function_3():
         father_robot.set_larm_rotation(90)
-        father_robot.set_larm_rotation(120)
-        time.sleep(1.2)
-        father_robot.set_larm_rotation(60)
+        father_robot.set_larm_rotation(120,3)
+        time.sleep(1.4)
+        father_robot.set_larm_rotation(60,3)
         father_robot.set_larm_rotation(90)
 
     def thread_function_4():
         father_robot.set_rarm_rotation(90)
-        father_robot.set_rarm_rotation(60)
-        time.sleep(1.3)
-        father_robot.set_rarm_rotation(120)
+        father_robot.set_rarm_rotation(60,3)
+        time.sleep(1.6)
+        father_robot.set_rarm_rotation(120,3)
         father_robot.set_rarm_rotation(90)
 
     thread1 = threading.Thread(target=thread_function_1, args=())
@@ -287,11 +287,11 @@ def curious_action(father_robot: robot.ROBOT):
     thread4 = threading.Thread(target=thread_function_4, args=())
 
     thread1.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread2.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread3.start()
-    time.sleep(0.1)
+    time.sleep(0.2)
     thread4.start()
 
 
